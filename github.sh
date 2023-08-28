554ec280c376313ad06246b07cb7d73d58a3e9e0
teAllvs <mateusallvezw095@gmail.com>
2023-08-28 10:16:48 -0300

    branch com o código da aula do dia Mon, Aug 28, 2023 10:16:48 AM

--- github.sh
+++ github.sh
@@ -2,9 +2,9 @@
 
 #Altere as variáveis abaixo de acordo com a sua necessidade
 #na variável repositório, prefira a url do repositório baseada em https para nao termos que configurar SSH
-export name=''
-export email=""
-export repositorio="";
+export name='teAllvs'
+export email="mateusallvezw095@gmail.com"
+export repositorio="https://github.com/teAllvs/mateusprova.git";
 
 #Daqui pra baixo mexa somente se for realmente necessário ou se souber o que está fazendo :P
 
